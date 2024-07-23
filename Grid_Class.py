294af2656d4a393072a23ac027f46a9c7126d2ce
Alfie-1516 <okorochaalfred@gmail.com>
2024-07-23 17:39:38 -0500

    Created Grid

--- Grid_Class.py
+++ Grid_Class.py
@@ -1,3 +1,35 @@
 """The Grid class manages a 20x10 grid for a Tetris game, handling grid initialization, checking cell status,
 clearing and moving rows, resetting the grid, and drawing the grid on a Pygame screen.
 """
+import pygame
+
+
+class Grid:
+    def __init__(self):
+        self.num_rows = 20
+        self.num_cols = 10
+        self.cell_size = 30
+        self.grid = [[0 for j in range(self.num_cols)] for i in range(self.num_rows)]
+        self.colors = self.get_cell_colors()
+
+    def get_cell_colors(self):
+        dark_gray = (26, 31, 40)
+        green = (47, 230, 23)
+        red = (232, 18, 18)
+        orange = (226, 116, 17)
+        yellow = (237, 234, 4)
+        purple = (166, 0, 247)
+        cyan = (21, 204, 209)
+        blue = (13, 64, 216)
+        white = (255, 255, 255)
+        dark_blue = (44, 44, 127)
+        light_blue = (59, 85, 162)
+        return [dark_gray, green, red, orange, yellow, purple, cyan, blue]
+
+    def draw(self, screen):
+        for row in range(self.num_rows):
+            for column in range(self.num_cols):
+                cell_value = self.grid[row][column]
+                cell_rect = pygame.Rect(column * self.cell_size  +1, row * self.cell_size +1, self.cell_size-1,
+                                        self.cell_size-1)
+                pygame.draw.rect(screen, self.colors[cell_value], cell_rect)
--- User_Interface.py
+++ User_Interface.py
@@ -1,1 +1,26 @@
 """This code sets up a Tetris game using Pygame, initializing necessary modules, fonts, and display elements."""
+import sys
+import pygame
+from Grid_Class import Grid
+
+pygame.init()
+dark_blue = (44, 44, 127)
+
+screen = pygame.display.set_mode((300, 600))
+pygame.display.set_caption("Cohort 3 Tetris")
+
+clock = pygame.time.Clock()
+
+game_grid = Grid()
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill(dark_blue)
+    game_grid.draw(screen)
+    pygame.display.update()
+    clock.tick(60)
